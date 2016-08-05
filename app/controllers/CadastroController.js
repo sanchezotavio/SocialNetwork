9e1e99b32789b7b529607aff30d0c4184bd462ff
Ótávio Sanchez <otavio@osanchez.com.br>
2016-08-05 17:31:53 -0300

    melhoria css

--- app/controllers/CadastroController.js
+++ app/controllers/CadastroController.js
@@ -1,5 +1,6 @@
 app.controller('CadastroController', function($rootScope, $location) {
 
     $rootScope.activetab = $location.path();
+    $rootScope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
 
 })
